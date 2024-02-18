8acd2db4d186d2307198ba35197d406052a76965
Diego Xavier <diegoxavier392@gmail.com>
2024-02-18 01:47:08 +0000

    Update Switch

--- src/main/java/sintaxe/intro/Switch.java
+++ src/main/java/sintaxe/intro/Switch.java
@@ -2,7 +2,7 @@ package sintaxe.intro;
 
 public class Switch {
     public static void main(String[] args) {
-        int day = 4;
+        int day = 5;
         switch (day) {
             case 1:
                 System.out.println("Monday");
