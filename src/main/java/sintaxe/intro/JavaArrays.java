0e48b5ab8d95bc658aa9a910be3bc27e1bb7ef30
Diego Xavier <diegoxavier392@gmail.com>
2024-02-18 21:00:05 +0000

    Update Java Arrays

---
+++ src/main/java/sintaxe/intro/JavaArrays.java
@@ -0,0 +1,9 @@
+package sintaxe.intro;
+
+public class JavaArrays {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars.length);
+
+    }
+}
