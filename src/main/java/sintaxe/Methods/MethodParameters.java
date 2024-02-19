29e9c8cab39ea1d0c2f1ac50bb1d29ea2ae2cc70
Diego Xavier <diegoxavier392@gmail.com>
2024-02-19 15:45:17 +0000

    Update MethodParamenters

---
+++ src/main/java/sintaxe/Methods/MethodParameters.java
@@ -0,0 +1,13 @@
+package sintaxe.Methods;
+
+public class MethodParameters {
+  static void myMethods(String fname) {
+        System.out.println(fname + " Refsnes");
+    }
+    public static void main(String[] args) {
+        myMethods("Diego");
+        myMethods("Marcone");
+        myMethods("Angelina");
+
+    }
+}
