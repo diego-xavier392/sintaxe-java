09d684b348d8b31f2b7a0fc57bfb903955a344c5
Diego Xavier <diegoxavier392@gmail.com>
2024-02-19 16:50:02 +0000

    Update Scope and Recursion

---
+++ src/main/java/sintaxe/Methods/Scope.java
@@ -0,0 +1,11 @@
+package sintaxe.Methods;
+
+public class Scope {
+    public static void main(String[] args){
+        {
+            int x = 100;
+            System.out.println(x);
+        }
+
+    }
+ }
