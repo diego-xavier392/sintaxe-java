523763746d9c146660dd270d6ca62f05683b1c76
Diego Xavier <diegoxavier392@gmail.com>
2024-02-26 18:41:39 +0000

    Update JavaPackagesAPI

---
+++ src/main/java/sintaxe/classes/JavaPackagesAPI.java
@@ -0,0 +1,16 @@
+package sintaxe.classes;
+
+import java.util.Scanner;
+public class JavaPackagesAPI {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        String userName;
+
+        System.out.println("Enter username");
+        userName = myObj.nextLine();
+
+        System.out.println( "Username is: " + userName);
+        
+
+    }
+}
